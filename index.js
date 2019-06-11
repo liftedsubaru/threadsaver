94ba2de34e9f8570bdd51acd18db7f594c7350f7
devgorilla <dwbrown89@gmail.com>
2019-06-10 20:20:15 -0700

    notes

--- index.js
+++ index.js
@@ -9,6 +9,41 @@ async function waybackCheck(event) {
   return _.get(archiveCheck, 'archived_snapshots.closest.url')
 }
 
+
+/*
+This likely needs to be more than just this lambda
+  Roadmap:
+
+  1. check for each page's existence and build list of ones to request.
+    a. paginate
+      - maybe build map of site:pages by trying known combos to find the correct one
+        (this is a big ?. some forums take params with page numbers
+        others an item count IE: theSamba, &start=440. (offset)
+        pirate4x4 does not use params at all, the page name is altered.
+        Perhaps solution should be more of a crawler based on page content than api)
+      - allow users to enter page params
+    b. request archives
+    c. return entire list of archive links.
+  2. create git commit.
+      a. git site repo
+      b. create rando branch
+      c. build page with input data + archive links (array).
+          - need to change styling.
+          - need to grab images for thumbnail, possibly resize/optimize, find an api somewhere.
+            JIMP! https://www.npmjs.com/package/jimp
+
+
+  v1. Should be biased towards archiving, not just submitting links.
+
+*/
+
+
+
+
+
+
+
+
 /*
   event == { url, pageCount }
 */
@@ -19,7 +54,7 @@ async function handler(event, context) {
   if (archiveUrl) return context.done(archiveUrl);
 
   // request some archives
-  //TODO: This doesnt work the way you think it does
+  // TODO: This doesnt work the way you think it does
   const resourceInfo = urlParser(event.url);
   const paginationString = config.pagination[resourceInfo.tokenized[1]]; // .sld should return 'google' from www.google.com
 
